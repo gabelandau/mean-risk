e99e22e26223c62bec131fbfd25d73583ca54d3b
Gabe Landau <gabelandau14@gmail.com>
2016-10-30 19:34:42 -0400

    Updated templates

--- dev/js/core.js
+++ dev/js/core.js
@@ -5,4 +5,12 @@ app.controller('brothers', function($scope, $http) {
     $http.get('http://localhost:3000/api/v1/brothers').then(function(response) {
         $scope.brothers = response.data.brothers;
     });
+
+    $scope.addPoint = function(index) {
+        $scope.brothers[index].points += 1;
+    }
+
+    $scope.subPoint = function(index) {
+        $scope.brothers[index].points -= 1;
+    }
 });
