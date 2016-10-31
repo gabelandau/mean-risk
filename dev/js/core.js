37ad738c8ae5725187b63bd07acddbb0f36a6912
Gabe Landau <gabelandau14@gmail.com>
2016-10-31 17:57:57 -0400

    Completed add brother form (no validation)

--- dev/js/core.js
+++ dev/js/core.js
@@ -2,7 +2,9 @@
 app = angular.module('risk', []);
 
 app.controller('brothers', function($scope, $http) {
-    $http.get('http://localhost:3000/api/v1/brothers').then(function(response) {
+    $scope.addSuccess = true;
+
+    $http.get('/api/v1/brothers').then(function(response) {
         $scope.brothers = response.data.brothers;
     });
 
@@ -12,5 +14,23 @@ app.controller('brothers', function($scope, $http) {
 
     $scope.subPoint = function(index) {
         $scope.brothers[index].points -= 1;
+    }
+
+    $scope.addBrother = function() {
+        $http({
+            method: 'post',
+            url: '/api/v1/brother',
+            data: {
+                'name': $scope.add.name,
+                'initiation_number': $scope.add.initiation_number,
+                'points': $scope.add.points,
+                'executive_council': $scope.add.executive_council,
+                'coop': $scope.add.coop,
+                'senior': $scope.add.senior
+            }
+        }).
+        then(function(response) {
+            $scope.addSuccess = false;
+        });
     }
 });
