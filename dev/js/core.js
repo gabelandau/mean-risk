ec524fd78bcb4415ad84c552a961542bb337985f
Gabe Landau <gabelandau14@gmail.com>
2016-10-31 18:06:59 -0400

    Minor changes to add form and Angular code

--- dev/js/core.js
+++ dev/js/core.js
@@ -3,6 +3,7 @@ app = angular.module('risk', []);
 
 app.controller('brothers', function($scope, $http) {
     $scope.addSuccess = true;
+    $scope.addFail = true;
 
     $http.get('/api/v1/brothers').then(function(response) {
         $scope.brothers = response.data.brothers;
@@ -14,6 +15,12 @@ app.controller('brothers', function($scope, $http) {
 
     $scope.subPoint = function(index) {
         $scope.brothers[index].points -= 1;
+    }
+
+    $scope.clearAddForm = function() {
+        $scope.addSuccess = true;
+        $scope.addFail = true;
+        $scope.add = null;
     }
 
     $scope.addBrother = function() {
@@ -30,7 +37,11 @@ app.controller('brothers', function($scope, $http) {
             }
         }).
         then(function(response) {
-            $scope.addSuccess = false;
+            if (response.status == 200) {
+                $scope.addSuccess = false;
+            } else {
+                $scope.addFail = false;
+            }
         });
     }
 });
